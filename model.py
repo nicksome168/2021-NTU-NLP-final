5e227cbcac8001a176239371b2b537a2b6626ae1
nicksome168 <b05502058@ntu.edu.tw>
2021-05-30 11:19:12 +0800

    create XLNet model class

--- model.py
+++ model.py
@@ -9,9 +9,9 @@ from transformers.modeling_outputs import MultipleChoiceModelOutput
 from transformers.models.xlnet.modeling_xlnet import XLNetForMultipleChoiceOutput
 # Creating the customized model, by adding a drop out and a dense layer on top of distil bert to get the final output for the model. 
 
-class MultipleChoiceModel(torch.nn.Module):
+class BertMultipleChoiceModel(torch.nn.Module):
     def __init__(self, model, d_model=768):
-        super(MultipleChoiceModel, self).__init__()
+        super(BertMultipleChoiceModel, self).__init__()
         self.model = model
         self.base = AutoModel.from_pretrained(model)
         # bert or longformer
@@ -61,48 +61,108 @@ class MultipleChoiceModel(torch.nn.Module):
             output_attentions=output_attentions,
             output_hidden_states=output_hidden_states,
         )
+    
+        pooled_output = outputs[1]
 
-        if 'xlnet' in self.model:
-            output = outputs[0]
+        pooled_output = self.dropout(pooled_output)
+        logits = self.classifier(pooled_output)
+        reshaped_logits = logits.view(-1, num_choices)
 
-            output = self.sequence_summary(output)
-            logits = self.classifier(output)
+        loss = None
+        if labels is not None:
+            loss_fct = CrossEntropyLoss()
+            loss = loss_fct(reshaped_logits, labels)
 
-            print(len(output))  
-            print(output.shape)  # torch.Size([3, 768])
+        return MultipleChoiceModelOutput(
+            loss=loss,
+            logits=reshaped_logits,
+            hidden_states=outputs.hidden_states,
+            attentions=outputs.attentions,
+        )
 
-            reshaped_logits = logits.view(-1, num_choices)
+class XLNetMultipleChoiceModel(torch.nn.Module):
+    def __init__(self, model, mem_len, d_model=768):
+        super(XLNetMultipleChoiceModel, self).__init__()
+        self.model = model
+        self.base = AutoModel.from_pretrained(model, mem_len=mem_len)
+        # bert or longformer
+        self.dropout = torch.nn.Dropout(0.3)
+        # xlnet
+        self.sequence_summary = SequenceSummary(XLNetConfig)
+        # common layers
+        self.classifier = torch.nn.Linear(d_model, 1)
 
-            loss = None
-            if labels is not None:
-                loss_fct = CrossEntropyLoss()
-                loss = loss_fct(reshaped_logits, labels.view(-1))
+    def forward(
+        self,
+        input_ids=None,
+        token_type_ids=None,
+        input_mask=None,
+        attention_mask=None,
+        mems=None,
+        perm_mask=None,
+        target_mapping=None,
+        head_mask=None,
+        inputs_embeds=None,
+        labels=None,
+        use_mems=None,
+        output_attentions=None,
+        output_hidden_states=None,
+        return_dict=None,
+    ):
+        r"""
+        labels (:obj:`torch.LongTensor` of shape :obj:`(batch_size,)`, `optional`):
+            Labels for computing the multiple choice classification loss. Indices should be in ``[0, ...,
+            num_choices-1]`` where :obj:`num_choices` is the size of the second dimension of the input tensors. (See
+            :obj:`input_ids` above)
+        """
 
-            return XLNetForMultipleChoiceOutput(
-                loss=loss,
-                logits=reshaped_logits,
-                mems=outputs.mems,
-                hidden_states=outputs.hidden_states,
-                attentions=outputs.attentions,
-            )
+        num_choices = input_ids.shape[1] if input_ids is not None else inputs_embeds.shape[1]
 
-        elif 'bert' in self.model:
-            pooled_output = outputs[1]
+        flat_input_ids = input_ids.view(-1, input_ids.size(-1)) if input_ids is not None else None
+        flat_token_type_ids = token_type_ids.view(-1, token_type_ids.size(-1)) if token_type_ids is not None else None
+        flat_attention_mask = attention_mask.view(-1, attention_mask.size(-1)) if attention_mask is not None else None
+        flat_input_mask = input_mask.view(-1, input_mask.size(-1)) if input_mask is not None else None
+        flat_inputs_embeds = (
+            inputs_embeds.view(-1, inputs_embeds.size(-2), inputs_embeds.size(-1))
+            if inputs_embeds is not None
+            else None
+        )
 
-            pooled_output = self.dropout(pooled_output)
-            logits = self.classifier(pooled_output)
-            reshaped_logits = logits.view(-1, num_choices)
+        outputs = self.base(
+            flat_input_ids,
+            token_type_ids=flat_token_type_ids,
+            input_mask=flat_input_mask,
+            attention_mask=flat_attention_mask,
+            mems=mems,
+            perm_mask=perm_mask,
+            target_mapping=target_mapping,
+            head_mask=head_mask,
+            inputs_embeds=flat_inputs_embeds,
+            use_mems=use_mems,
+            output_attentions=output_attentions,
+            output_hidden_states=output_hidden_states,
+            return_dict=return_dict,
+        )
 
-            loss = None
-            if labels is not None:
-                loss_fct = CrossEntropyLoss()
-                loss = loss_fct(reshaped_logits, labels)
+        output = outputs[0]
 
-            output = (reshaped_logits,) + outputs[2:]
+        output = self.sequence_summary(output)
+        logits = self.classifier(output)
 
-            return MultipleChoiceModelOutput(
-                loss=loss,
-                logits=reshaped_logits,
-                hidden_states=outputs.hidden_states,
-                attentions=outputs.attentions,
-            )
+        # print(len(output))  
+        # print(output.shape)  # torch.Size([3, 768])
+
+        reshaped_logits = logits.view(-1, num_choices)
+
+        loss = None
+        if labels is not None:
+            loss_fct = CrossEntropyLoss()
+            loss = loss_fct(reshaped_logits, labels.view(-1))
+
+        return XLNetForMultipleChoiceOutput(
+            loss=loss,
+            logits=reshaped_logits,
+            mems=outputs.mems,
+            hidden_states=outputs.hidden_states,
+            attentions=outputs.attentions,
+        )
