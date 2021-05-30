abd17df3c100ba0c0e0b0d7cfc6ffb424443d7c2
nicksome168 <b05502058@ntu.edu.tw>
2021-05-30 11:50:57 +0800

    add ensemble training code

--- dataset.py
+++ dataset.py
@@ -6,11 +6,15 @@ from torch.utils.data import Dataset
 from transformers import AutoTokenizer
 
 class QADataset(Dataset):
-    def __init__(self, data: list, tokenizer: AutoTokenizer, max_seq_length: int, mode: str):
+    def __init__(self, data: list, tokenizer: AutoTokenizer, max_seq_length: int, mode: str, ensemble: bool, pg_stride: int=435, max_pg_len: int=450):
         self._data = data
         self.tokenizer = tokenizer
         self.max_seq_length = max_seq_length
         self.mode = mode
+        self.ensemble =ensemble
+        self.max_pg_len = max_pg_len
+        self.pg_stride = pg_stride
+        
     def __len__(self) -> int:
         return len(self._data)
 
@@ -34,22 +38,67 @@ class QADataset(Dataset):
                 torch.stack(attention_mask_batch), \
                 torch.stack(label_batch)
     
-    def trim_to_max_length(self, max_seq_length: int, passage):
-        if len(passage) > max_seq_length - 3:
-            passage = passage[-(max_seq_length - 3):]
-        return passage
+    # def trim_to_max_length(self, max_seq_length: int, passage):
+    #     if len(passage) > 2000:
+    #         passage = passage[:2000]
+    #     return passage
 
     def preprocess(self, tokenizer, max_seq_length: int, x) -> Dict:
-        choices_features = []
         label_map = {"A": 0, "B": 1, "C": 2}
         question = x["question"]["stem"]
         passage = x["text"]
-        passage = self.trim_to_max_length(max_seq_length, passage)
 
-        option: str
-        for option in x["question"]["choices"]:    
-            question_option = question + " " + option['text']
+        if self.ensemble:
+            all_splits_choices_features = {"input_ids":[], "attention_mask":[]}
+            # max len of ques+opt = 51 
+            # max len of pg = 7654
+            option: str
+            for option in x["question"]["choices"]:
+                splits_choices_features = []    
+                question_option = question + " " + option['text'] 
+                # max_len = 3060
+                # len of pg per split = 450
+                # stride = 435 (i.e., 15 tokens overlapped)
+                for i in range(0, self.pg_stride * 6, self.pg_stride):
+                    passage_split = x["text"][i : i + self.max_pg_len]
+                    inputs = tokenizer(
+                        passage_split,
+                        question_option,
+                        add_special_tokens=True,
+                        max_length=max_seq_length,
+                        truncation=True,
+                        padding='max_length',
+                        return_tensors='pt'
+                    )
+                    
+                    splits_choices_features.append(inputs)
+                
+                all_splits_choices_features["input_ids"].append(  \
+                    torch.vstack([cf["input_ids"][0] for cf in splits_choices_features]).permute(1,0)
+                )
+                all_splits_choices_features["attention_mask"].append(  \
+                    torch.vstack([cf["attention_mask"][0] for cf in splits_choices_features]).permute(1,0)
+                )
 
+            if self.mode == "test":
+                label = None
+            else:
+                labels = label_map.get(x["answer"].strip(), -1)
+                label = torch.tensor(labels).long()
+
+            # shape of input_ids and attention_mask in return = [num_options, max_seq_len, num_splits]
+            return {
+                "label": label,
+                "input_ids": torch.stack([ids for ids in all_splits_choices_features["input_ids"]], dim=0),
+                "attention_mask": torch.stack([mask for mask in all_splits_choices_features["attention_mask"]], dim=0),
+    #             "token_type_ids": torch.cat([cf["token_type_ids"] for cf in choices_features]).reshape(-1),
+            }
+                
+        else:
+            choices_features = []
+            option: str
+            for option in x["question"]["choices"]:    
+                question_option = question + " " + option['text']
             inputs = tokenizer(
                 passage,
                 question_option,
@@ -61,16 +110,16 @@ class QADataset(Dataset):
             )
 
             choices_features.append(inputs)
-        
-        if self.mode == "test":
-            label = None
-        else:
-            labels = label_map.get(x["answer"].strip(), -1)
-            label = torch.tensor(labels).long()
             
-        return {
-            "label": label,
-            "input_ids": torch.stack([cf["input_ids"] for cf in choices_features]).reshape(-1, max_seq_length),
-            "attention_mask": torch.stack([cf["attention_mask"] for cf in choices_features]).reshape(-1, max_seq_length),
-#             "token_type_ids": torch.cat([cf["token_type_ids"] for cf in choices_features]).reshape(-1),
-        }
+            if self.mode == "test":
+                label = None
+            else:
+                labels = label_map.get(x["answer"].strip(), -1)
+                label = torch.tensor(labels).long()
+                    
+            return {
+                "label": label,
+                "input_ids": torch.stack([cf["input_ids"] for cf in choices_features]).reshape(-1, max_seq_length),
+                "attention_mask": torch.stack([cf["attention_mask"] for cf in choices_features]).reshape(-1, max_seq_length),
+    #             "token_type_ids": torch.cat([cf["token_type_ids"] for cf in choices_features]).reshape(-1),
+            }
--- train_qa.py
+++ train_qa.py
@@ -4,6 +4,7 @@ import json
 from pathlib import Path
 
 import torch
+from torch.nn import CrossEntropyLoss
 from torch.utils.data import DataLoader
 from torch.optim import AdamW
 from transformers import AutoTokenizer
@@ -11,7 +12,7 @@ from tqdm import tqdm
 
 from utils import handle_reproducibility, loss_fn, clean_data
 from dataset import QADataset
-from model import MultipleChoiceModel
+from model import BertMultipleChoiceModel, XLNetMultipleChoiceModel
 
 
 def train(args: argparse.Namespace) -> None:
@@ -19,14 +20,15 @@ def train(args: argparse.Namespace) -> None:
         all_data = json.load(file)
     all_data = clean_data(all_data)
     
-    # Split data
+    # Split Data
     valid_data = all_data[:round(len(all_data) * args.train_val_split)]
     train_data = all_data[round(len(all_data) * args.train_val_split):]
     print(f"train data: {len(train_data)} valid data: {len(valid_data)}")
 
+    # Load Dataset
     tokenizer = AutoTokenizer.from_pretrained(args.base_model)
-    train_set = QADataset(train_data, tokenizer, args.max_seq_length, mode="train")
-    valid_set = QADataset(valid_data, tokenizer, args.max_seq_length, mode="valid")
+    train_set = QADataset(train_data, tokenizer, args.max_seq_length, mode="train", ensemble=args.ensemble)
+    valid_set = QADataset(valid_data, tokenizer, args.max_seq_length, mode="valid", ensemble=args.ensemble)
 
     train_loader = DataLoader(
         train_set,
@@ -47,11 +49,18 @@ def train(args: argparse.Namespace) -> None:
         pin_memory=True,
     )
 
-    model = MultipleChoiceModel(args.base_model)
+    # Initiate Model
+    if 'xlnet' in args.base_model:
+        model = XLNetMultipleChoiceModel(args.base_model, mem_len=args.max_seq_length)
+    else:
+        model = BertMultipleChoiceModel(args.base_model)
     model.to(args.device)
-
+    
+    # Optimizer and Loss func
     optimizer = AdamW(model.parameters(), lr=args.lr, weight_decay=args.wd)
+    loss_fct = CrossEntropyLoss()
 
+    # Logging
     if args.wandb_logging:
         import wandb
 
@@ -67,17 +76,33 @@ def train(args: argparse.Namespace) -> None:
         train_loss = 0
         train_corrects = 0
         for batch_idx, (input_ids_batch, attention_mask_batch, label_batch) in enumerate(tqdm(train_loader)):
-            # input shape = (batch_size, num_options, seq_len)
-            # label shape = (batch_size,)
-            
             input_ids = input_ids_batch.to(args.device)
             attention_mask = attention_mask_batch.to(args.device)
             labels = label_batch.to(args.device)
-
-            outputs = model(input_ids=input_ids, attention_mask=attention_mask, labels=labels)
             
-            loss = outputs.loss
-            logits = outputs.logits
+            # DEBUG
+            # print(tokenizer.decode(input_ids[0,0,:,0]))
+            # print(input_ids.shape)
+
+            if args.ensemble:
+                # input shape = (batch_size, num_options, seq_len, num_split)
+                # label shape = (batch_size,)
+                mems, loss = None, None
+                logits = torch.zeros((args.batch_size, input_ids_batch.shape[1])).to(args.device)  # [batch_size, num_options]
+                splits_num = input_ids.shape[3]
+                for i in range(splits_num):
+                    outputs = model(input_ids=input_ids[:,:,:,i], attention_mask=attention_mask[:,:,:,i], labels=labels, mems=mems)
+                    mems = outputs.mems  # XLNet hidden_state
+                    logits += outputs.logits
+                logits /= splits_num
+                loss = loss_fct(logits, labels.view(-1))
+            else:
+                # input shape = (batch_size, num_options, seq_len)
+                # label shape = (batch_size,)
+                outputs = model(input_ids=input_ids, attention_mask=attention_mask, labels=labels)
+                loss = outputs.loss
+                logits = outputs.logits
+            
             loss.backward()
             if (batch_idx + 1) % args.n_batch_per_step == 0:
                 optimizer.step()
@@ -103,7 +128,20 @@ def train(args: argparse.Namespace) -> None:
                 attention_mask = attention_mask_batch.to(args.device)
                 labels = label_batch.to(args.device)
                 
-                outputs = model(input_ids=input_ids, attention_mask=attention_mask, labels=labels)
+                if args.ensemble:
+                    mems, loss = None, None
+                    logits = torch.zeros((args.batch_size, input_ids_batch.shape[1])).to(args.device)  # [batch_size, num_options]
+                    splits_num = input_ids.shape[3]
+                    for i in range(splits_num):
+                        outputs = model(input_ids=input_ids[:,:,:,i], attention_mask=attention_mask[:,:,:,i], labels=labels, mems=mems)
+                        mems = outputs.mems
+                        logits += outputs.logits
+                    logits /= splits_num
+                    loss = loss_fct(logits, labels.view(-1))
+                else:
+                    outputs = model(input_ids=input_ids, attention_mask=attention_mask, labels=labels)
+                    loss = outputs.loss
+                    logits = outputs.logits
 
                 valid_loss += loss.item()
                 logits = outputs.logits
@@ -146,7 +184,7 @@ def parse_args() -> argparse.Namespace:
     )
 
     # model
-    parser.add_argument("--base-model", type=str, default="bert-base-chinese") #allenai/longformer-base-4096
+    parser.add_argument("--base_model", type=str, default="bert-base-chinese") #allenai/longformer-base-4096
     parser.add_argument(
         "--model_dir",
         type=Path,
@@ -159,6 +197,7 @@ def parse_args() -> argparse.Namespace:
     parser.add_argument("--wd", type=float, default=1e-2)
 
     # data loader
+    parser.add_argument("--ensemble", type=bool, default=False)
     parser.add_argument("--batch_size", type=int, default=8)
     parser.add_argument("--train_val_split", type=int, default=0.1)
     parser.add_argument("--max_seq_length", type=int, default=512)
@@ -183,5 +222,5 @@ if __name__ == "__main__":
 
     args = parse_args()
     args.model_dir.mkdir(parents=True, exist_ok=True)
-
+    
     train(args)
